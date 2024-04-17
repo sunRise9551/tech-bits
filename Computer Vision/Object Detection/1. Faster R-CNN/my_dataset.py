83ef31d184bd7f95491f937f858e5f1792c84193
sunRise9551 <wongkiesun@gmail.com>
2024-04-17 14:08:32 +0800

    vocdataset_getitem

--- Computer Vision/Object Detection/1. Faster R-CNN/my_dataset.py
+++ Computer Vision/Object Detection/1. Faster R-CNN/my_dataset.py
@@ -9,7 +9,7 @@ from lxml import etree
 
 class VOCDataset(Dataset):
     """ Read and Parse VOC Dataset"""
-    def __init__(self, voc_root, transforms, tran_set=True):
+    def __init__(self, voc_root, transforms=None, tran_set=True):
         self.root = os.path.join(voc_root, "VOCdevkit", "VOC2012")
         self.annotations_root = os.path.join(self.root, "Annotations")
         self.img_root = os.path.join(self.root, "JPEGImages")
@@ -32,10 +32,7 @@ class VOCDataset(Dataset):
             print(e)
             exit(1)
 
-        self.transfroms = transforms
-
-
-        pass
+        self.transforms = transforms
 
     def __len__(self):
         return len(self.xml_list)
@@ -46,7 +43,55 @@ class VOCDataset(Dataset):
             xml_str = fid.read()
         xml = etree.fromstring(xml_str)
         data = self.parse_xml_to_dict(xml)["annotation"]
+        img_path = os.path.join(self.img_root, data["filename"])
+        image = Image.open(img_path)
 
+        if image.format != "JPEG":
+            raise ValueError("Image format is not JPEG")
+        boxes = []
+        labels = []
+        iscrowd = [] # Use to store each <object>'s details
+        for obj in data["object"]:
+            xmin = float(obj["bndbox"]["xmin"])
+            ymin = float(obj["bndbox"]["ymin"])
+            xmax = float(obj["bndbox"]["xmax"])
+            ymax = float(obj["bndbox"]["ymax"])
+            boxes.append([xmin, ymin, xmax, ymax])
+            labels.append(self.class_dict[obj["name"]])
+            iscrowd.append(int(obj["difficult"]))
+
+        # convert everything to tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        labels = torch.as_tensor(labels, dtype=torch.float32)
+        iscrowd = torch.as_tensor(iscrowd, dtype=torch.float32)
+        image_id = torch.tensor([index])
+
+        # Area Formula
+        # (ymax-ymin) * (xmax-xmin)
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            image, target = self.transforms(image, target)
+
+        return image, target
+
+    def get_height_and_width(self, idx):
+        # read xml
+        xml_path = self.xml_list[idx]
+        with open(xml_path) as fid:
+            xml_str = fid.read()
+        xml = etree.fromstring(xml_str)
+        data = self.parse_xml_to_dict(xml)["annotation"]
+        data_height = int(data["size"]["height"])
+        data_width = int(data["size"]["width"])
+        return data_height, data_width
 
     def parse_xml_to_dict(self, xml):
         """
@@ -67,3 +112,46 @@ class VOCDataset(Dataset):
                     result[child.tag] = []
                 result[child.tag].append(child_result[child.tag])
         return {xml.tag: xml.text}
+
+
+# import transforms
+# from draw_box_utils import draw_objs
+# from PIL import Image
+# import json
+# import matplotlib.pyplot as plt
+# import torchvision.transforms as ts
+# import random
+#
+# # read class_indict
+# category_index = {}
+# try:
+#     json_file = open('./pascal_voc_classes.json', 'r')
+#     class_dict = json.load(json_file)
+#     category_index = {str(v): str(k) for k, v in class_dict.items()}
+# except Exception as e:
+#     print(e)
+#     exit(-1)
+#
+# data_transform = {
+#     "train": transforms.Compose([transforms.ToTensor(),
+#                                  transforms.RandomHorizontalFlip(0.5)]),
+#     "val": transforms.Compose([transforms.ToTensor()])
+# }
+#
+# # load train data set
+# train_data_set = VOCDataSet(os.getcwd(), "2012", data_transform["train"], "train.txt")
+# print(len(train_data_set))
+# for index in random.sample(range(0, len(train_data_set)), k=5):
+#     img, target = train_data_set[index]
+#     img = ts.ToPILImage()(img)
+#     plot_img = draw_objs(img,
+#                          target["boxes"].numpy(),
+#                          target["labels"].numpy(),
+#                          np.ones(target["labels"].shape[0]),
+#                          category_index=category_index,
+#                          box_thresh=0.5,
+#                          line_thickness=3,
+#                          font='arial.ttf',
+#                          font_size=20)
+#     plt.imshow(plot_img)
+#     plt.show()
